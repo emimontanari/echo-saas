815f97b40a64a448d04a3d94ac00674107a54414
emimontanari <emimontanari@icloud.com>
2025-11-06 11:41:28 -0300

    04: organizations

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,6 +1,11 @@
 "use client";
-import { Authenticated, Unauthenticated, useMutation, useQuery } from "convex/react";
-import { SignInButton, UserButton } from "@clerk/nextjs";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
+import { OrganizationSwitcher, SignInButton, UserButton } from "@clerk/nextjs";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 
@@ -13,12 +18,13 @@ export default function Page() {
       <Authenticated>
         <div className="flex flex-col items-center justify-center min-h-svh">
           <UserButton />
+          <OrganizationSwitcher hidePersonal />
           <Button onClick={() => addUser()}>Add</Button>
           <div className="max-w-sm w-full mx-auto">
             {JSON.stringify(users, null, 2)}
           </div>
         </div>
-      </Authenticated >
+      </Authenticated>
       <Unauthenticated>
         <SignInButton>
           <Button>Sign In</Button>
