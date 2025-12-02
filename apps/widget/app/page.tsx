1b7f6d0213eac9784e03d159006e872f70869c81
emimontanari <emimontanari@icloud.com>
2025-12-02 10:38:42 -0300

    09: widget layout

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,26 +1,17 @@
 "use client";
 
-import { useVapi } from "@/modules/widget/hooks/use-vapi";
 import { Button } from "@workspace/ui/components/button";
+import { WidgetView } from "@/modules/widget/ui/views/widget-view";
+import { use } from "react";
 
-export default function Page() {
-  const {
-    isConnected,
-    isConnecting,
-    isSpeaking,
-    transcript,
-    startCall,
-    endCall,
-  } = useVapi();
-  return (
-    <div className="flex flex-col items-center justify-center min-h-svh max-w-md mx-auto w-full">
-      <p>isConnected: {isConnected ? "true" : "false"}</p>
-      <p>isConnecting: {isConnecting ? "true" : "false"}</p>
-      <p>isSpeaking: {isSpeaking ? "true" : "false"}</p>
-      <Button onClick={startCall}>Start Call</Button>
-      <Button onClick={endCall}>End Call </Button>
-
-      <p>transcript: {JSON.stringify(transcript)}</p>
-    </div>
-  );
+interface Props {
+  searchParams: Promise<{ organizationId: string }>;
 }
+
+const Page = ({ searchParams }: Props) => {
+  const { organizationId } = use(searchParams);
+
+  return <WidgetView organizationId={organizationId} />;
+};
+
+export default Page;
---
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { WidgetFooter } from "../components/widget-footer";
+import { WidgetHeader } from "../components/widget-header";
+
+interface Props {
+  organizationId: string;
+}
+
+export const WidgetView = ({ organizationId }: Props) => {
+  return (
+    <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
+      <WidgetHeader>
+        <div className="flex flex-col justify-between gap-y-2 px-2 py-6">
+          <p className="font-semibold text-3xl">Hi There!👋🏻 </p>
+          <p className="text-lg">How can we help you today?</p>
+        </div>
+      </WidgetHeader>
+      Widget View : {organizationId}
+      <WidgetFooter />
+    </main>
+  );
+};
