0dd9e69e2e8b206a1c5c6387b277e595eaf97f49
emimontanari <emimontanari@icloud.com>
2025-12-18 14:59:24 -0300

    17: Dashboard Inbox

---
+++ apps/web/lib/country-utils.ts
@@ -0,0 +1,27 @@
+import * as ct from 'countries-and-timezones';
+
+export const getContryFromTimezone = (timezone: string | undefined) => {
+    if (!timezone) return null;
+
+    const timezoneInfo = ct.getTimezone(timezone);
+
+    if (!timezoneInfo?.countries) return null;
+
+    const countryCode = timezoneInfo.countries[0];
+
+    if (!countryCode) return null;
+
+    const country = ct.getCountry(countryCode);
+
+    return {
+        countryCode,
+        countryName: country?.name ?? 'Unknown'
+    };
+};
+
+
+
+export const getCountryFlagUrl = (countryCode: string) => {
+    return `https://flagcdn.com/w40/${countryCode.toLowerCase()}.png`;
+}
+
